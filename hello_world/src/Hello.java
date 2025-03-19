1d237c804909d5327be7b5400e9c2e7d2c1a019e
sakura <946788898@qq.com>
2025-03-20 00:14:17 +0800

    add hello world

---
+++ hello_world/src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("你好");
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("echo:"+in.nextLine())
+        System.out.println("2+3="+2+3);
+        System.out.println("2+3="+(2+3));
+    }
+}
