cd63a6c9261923df6ee42432b8532d32d7df4224
sakura <946788898@qq.com>
2025-03-20 11:04:24 +0800

    add files

--- hello_world/src/Hello.java
+++ hello_world/src/Hello.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-//        System.out.println("你好");
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("echo:"+in.nextLine())
-        System.out.println("2+3="+2+3);
-        System.out.println("2+3="+(2+3));
+        System.out.println("你好");
+        Scanner in = new Scanner(System.in);
+        int price,amount;
+        System.out.print("请输入价格:");
+        price=in.nextInt();
+        System.out.print("请输入票面：");
+        amount=in.nextInt();
+//        System.out.println("echo:"+in.nextLine());
+//        System.out.println("2+3="+2+3);
+//        System.out.println("2+3="+(2+3));
+        System.out.println(amount+"-"+price+"="+(amount-price));
     }
 }
