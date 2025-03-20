4d353f73ef92a1239598c67d1aef74227c4e4122
sakura <946788898@qq.com>
2025-03-20 20:23:52 +0800

    add files

---
+++ hello_world/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        double inch,foot;
+        System.out.print("请输入英尺");
+        foot=in.nextDouble();
+        System.out.print("请输入英寸");
+        inch=in.nextDouble();
+        System.out.println((int)(100*0.3046*(foot+inch/12))+"cm");
+    }
+}
